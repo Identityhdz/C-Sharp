f45c897a321f16420f0cc10ea6663576be746940
Igmar Jovan <Igmhdmtz@gmail.com>
2020-10-04 10:01:36 -0500

    Trabajado primer bloque de condicionales, agregadas Operaciones.cs

---
+++ C-Sharp Profesional/C-Sharp Profesional/Operaciones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Profesional
+{
+    class Operaciones
+    {
+
+        // Variables de operaciones
+        private int Suma;
+
+        #region Suma de dos valores
+        public void ObtenerValorSuma(int Numero_Int, int Numero_Int2)
+        {
+            Suma = Numero_Int + Numero_Int2;
+        }
+
+        public int ImprimirSuma()
+        {
+            return Suma;
+        }
+        #endregion
+    }
+}
--- C-Sharp Profesional/C-Sharp Profesional/Program.cs
+++ C-Sharp Profesional/C-Sharp Profesional/Program.cs
@@ -10,6 +10,52 @@ namespace C_Sharp_Profesional
     {
         static void Main(string[] args)
         {
+
+            // Variables Globales
+            string Cadena = "";
+            int Numero_1;
+            int Numero_2;
+
+            #region Bloque 4 | Condicionales
+
+            Console.WriteLine("Define Valor 1 : ");
+            Cadena = Console.ReadLine();
+            Numero_1 = Convert.ToInt32(Cadena);
+
+            Console.WriteLine("Define Valor 2 : ");
+            Cadena = Console.ReadLine();
+            Numero_2 = Convert.ToInt32(Cadena);
+
+
+            if (Numero_1 <= Numero_2) {
+                Console.WriteLine("El valor 1: {0} es menor o igual al Valor 2: {1}", Numero_1, Numero_2);
+            }
+            else {
+                Console.WriteLine("El Valor 2: {1} es mayor al Valor 1: {0}", Numero_1, Numero_2);
+                Console.ReadKey();
+            }
+
+            #endregion
+
+            #region Ejercicio 1| Suma de precios | Clase Operaciones
+            //Operaciones Operando = new Operaciones();
+
+            //Console.WriteLine("Define el Precio 1 $");
+            //Cadena = Console.ReadLine();
+            //Numero_1 = Convert.ToInt32(Cadena);
+
+            //Console.WriteLine("Define el Precio 2 $");
+            //Cadena = Console.ReadLine();
+            //Numero_2 = Convert.ToInt32(Cadena);
+
+            //Operando.ObtenerValorSuma(Numero_1, Numero_2);
+
+            //Console.WriteLine("La sumatoria de los precios es de: {0:C}", Operando.ImprimirSuma());
+            //Console.ReadKey();
+
+            #endregion
+
+
         }
     }
 }
