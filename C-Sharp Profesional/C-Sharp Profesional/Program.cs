46a25d2f93df4d0e806af89f4c3576befd8f73b8
Igmar Jovan <Igmhdmtz@gmail.com>
2020-10-04 23:34:01 -0500

    Bloque 4 agregado

--- C-Sharp Profesional/C-Sharp Profesional/Program.cs
+++ C-Sharp Profesional/C-Sharp Profesional/Program.cs
@@ -11,50 +11,149 @@ namespace C_Sharp_Profesional
         static void Main(string[] args)
         {
 
-            // Variables Globales
-            string Cadena = "";
-            int Numero_1;
-            int Numero_2;
+            // Variables GLOBALES
+
+            string esCadena = "";
+            int numeroUno;
+            int numeroDos;
 
             #region Bloque 4 | Condicionales
 
-            Console.WriteLine("Define Valor 1 : ");
-            Cadena = Console.ReadLine();
-            Numero_1 = Convert.ToInt32(Cadena);
+            Console.WriteLine ( "¿Cual es el valor de Numero Uno ?" );
+            esCadena = Console.ReadLine();
+            numeroUno = Convert.ToInt32(esCadena);
 
-            Console.WriteLine("Define Valor 2 : ");
-            Cadena = Console.ReadLine();
-            Numero_2 = Convert.ToInt32(Cadena);
+            Console.WriteLine ( "¿Cual es el valor de Numero Dos ?" );
+            esCadena = Console.ReadLine();
+            numeroDos = Convert.ToInt32(esCadena);
 
 
-            if (Numero_1 <= Numero_2) {
-                Console.WriteLine("El valor 1: {0} es menor o igual al Valor 2: {1}", Numero_1, Numero_2);
-            }
-            else {
-                Console.WriteLine("El Valor 2: {1} es mayor al Valor 1: {0}", Numero_1, Numero_2);
+            #region Condición If Anidado
+
+            if (numeroUno <= numeroDos) {
+                if (numeroUno == numeroDos) {
+                    Console.WriteLine ( "Numero Uno: {0} es igual a Numero Dos: {1}", numeroUno, numeroDos );
+                    Console.ReadKey();
+                } else {
+                    Console.WriteLine ( "Numero Uno: {0} es menor a Numero Dos: {1}", numeroUno, numeroDos );
+                    Console.ReadKey();
+                }
+            } else {
+                Console.WriteLine ( "Numero Uno: {0} es mayor a Numero Dos {1}", numeroUno, numeroDos );
                 Console.ReadKey();
             }
 
             #endregion
 
-            #region Ejercicio 1| Suma de precios | Clase Operaciones
-            //Operaciones Operando = new Operaciones();
+            #region Condición If Else
 
-            //Console.WriteLine("Define el Precio 1 $");
-            //Cadena = Console.ReadLine();
-            //Numero_1 = Convert.ToInt32(Cadena);
-
-            //Console.WriteLine("Define el Precio 2 $");
-            //Cadena = Console.ReadLine();
-            //Numero_2 = Convert.ToInt32(Cadena);
-
-            //Operando.ObtenerValorSuma(Numero_1, Numero_2);
-
-            //Console.WriteLine("La sumatoria de los precios es de: {0:C}", Operando.ImprimirSuma());
-            //Console.ReadKey();
+            if (numeroUno < numeroDos) {
+                Console.WriteLine( "Numero Uno: {0} es menor a Numero Dos: {1}", numeroUno, numeroDos );
+                Console.ReadKey();
+            } else if (numeroUno == numeroDos) {
+                Console.WriteLine( "Numero Uno: {0} es igual a Numero Dos: {1}", numeroUno, numeroDos );
+                Console.ReadKey();
+            } else {
+                Console.WriteLine( "Numero Uno: {0} es MENOR a Numero Dos: {1}", numeroUno, numeroDos );
+                Console.ReadKey();
+            }
 
             #endregion
 
+            #region Precedencia de Operadores
+
+            int esResultado = (numeroUno + numeroDos) * (numeroUno / numeroDos);
+
+            Console.WriteLine( "La operación da como resultado: {0}", esResultado);
+            Console.ReadKey();
+
+            #endregion
+
+            #region Opciones Multiples
+
+            switch (numeroUno) {
+                case 1:
+                    Console.WriteLine( "Se ha escojido el caso numero 1" );
+                    break;
+
+                case 2:
+                    Console.WriteLine( "Se ha escojido el caso numero 2" );
+                    break;
+
+                default:
+                    Console.WriteLine( "No se ha seleccionado ningun caso" );
+                    break;
+            }
+            Console.ReadKey();
+
+            #endregion
+
+            #region Valores Nulables y Operador Terniario
+
+            int? valorX = null;
+            int valorY;
+            
+            /*
+             * Aquí esta el operador terniario con sintaxis () ? :
+             */
+
+            string esCadenaString = valorX.HasValue ? "valorX ha devuelto como valor :{0}" + valorX.ToString() : "valorX ha devuelto un valor :{0}" ;
+
+            Console.WriteLine(esCadenaString);
+            Console.ReadKey();
+
+            /* 
+             * (??) En caso de que X tenga valor, se asgina a Y
+             * (?? -1) En caso de que X sea null se asgina un valor despues de ?? que es -1
+             */
+
+            valorY = valorX ?? -1; 
+
+            Console.WriteLine("valorY devuelve como valor: {0}", valorY );
+            Console.ReadKey();
+
+            #endregion
+
+            #endregion
+
+            #region Ejercicio 1| Suma de precios | Clase Operaciones
+
+            Operaciones tipoOperacion = new Operaciones();
+
+            Console.WriteLine( "Determina precio 1: " );
+            esCadena = Console.ReadLine();
+            numeroUno = Convert.ToInt32(esCadena);
+
+            Console.WriteLine( "Determina precio 2:");
+            esCadena = Console.ReadLine();
+            numeroDos = Convert.ToInt32(esCadena);
+
+            tipoOperacion.ObtenerValorSuma(numeroUno, numeroDos);
+
+            Console.WriteLine( "Al sumar el precio 1 y precio 2 se tiene un total de: {0:C} ", tipoOperacion.ImprimirSuma() );
+            Console.ReadKey();
+
+            #endregion
+
+            #region Ejercicio 2 | Condiciones | Bloque 4
+
+            string ejemploCadena = "";
+            int edadPersona = 0;
+
+            Console.WriteLine( "Hola, ¿cual es tu edad?" );
+            ejemploCadena = Console.ReadLine();
+            edadPersona = Convert.ToInt32(ejemploCadena);
+
+            if (edadPersona < 18) {
+                Console.WriteLine( "¡Uh! No tienes edad suficiente, lo lamento :( ", edadPersona);
+                Console.ReadKey();
+            }
+            else if (edadPersona >= 18) {
+                Console.WriteLine("¡Genial!, ya yienes edad sufiecente, chocalas :) ", edadPersona);
+                Console.ReadKey();
+            }
+
+            #endregion
 
         }
     }
